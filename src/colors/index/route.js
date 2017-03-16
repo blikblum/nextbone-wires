522954089d71be2f79da12dec489aeabca9042c1
Luiz Américo <brandkarl@gmail.com>
2017-03-16 19:18:54 -0300

    Ensure data is loaded for all colors children routes

--- src/colors/index/route.js
+++ src/colors/index/route.js
@@ -6,15 +6,18 @@ export default Route.extend({
   activate(transition) {
     let pageParam = transition.query.page;
     this.page = pageParam && parseFloat(pageParam) || 1;
-    return storage.findAll({ajaxSync: true}).then(collection => {
+    return storage.findAll().then(collection => {
       this.collection = collection;
     });
   },
 
   updateView(transition) {
+    // updateView is called when the route is already rendered (route.view !== undefined)
     let pageParam = transition.query.page;
     let page = pageParam && parseFloat(pageParam) || 1;
     this.view.updateState({page: page});
+    // the default behavior is the current view be replaced by a new view instance
+    // by returning true, the current view is is not destroyed / removed
     return true
   },
 
--- src/colors/route.js
+++ src/colors/route.js
@@ -3,8 +3,14 @@ import ColorsIndexRoute from './index/route';
 import ColorsShowRoute from './show/route';
 import ColorsEditRoute from './edit/route';
 import ColorsCreateRoute from './create/route';
+import storage from './storage';
 
 export default Route.extend({
+  activate() {
+    //ensure data is loaded for all children routes
+    return storage.findAll({ajaxSync: true});
+  },
+
   childRoutes: {
     'colors.index': ColorsIndexRoute,
     'colors.show': ColorsShowRoute,
