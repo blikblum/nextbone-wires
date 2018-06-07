8402274a0cf0970ee059ceb4d654e69bf09bd9bb
Luiz Américo <luizamericop@gmail.com>
2018-06-07 12:29:33 -0300

    Update webpack for v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: __dirname + '/src/main.js',
@@ -14,19 +14,53 @@ module.exports = {
       filename: __dirname + '/dist/index.html',
       template: __dirname + '/src/index.html'
     }),
-    // extract inline css into separate 'styles.css'
-    new ExtractTextPlugin('bundle.css')
+
+    new MiniCSSExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
   ],
   module: {
-    loaders: [
-      {test: /\.hbs$/, loader: 'handlebars'},
-      {test: /bootstrap.+\.js$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
-      {test: /\.js$/, loader: 'babel?presets[]=es2015', include: [path.resolve(__dirname, 'src')]},
-      {test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')},
-      {test: /\.(woff|woff2)$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      {test: /\.ttf$/,  loader: "url?limit=10000&mimetype=application/octet-stream" },
-      {test: /\.eot$/,  loader: "file" },
-      {test: /\.svg$/,  loader: "url?limit=10000&mimetype=image/svg+xml" }
+    rules: [
+      { 
+        test: /\.hbs$/, 
+        loader: 'handlebars-loader' 
+      },
+      { 
+        test: /bootstrap.+\.js$/, 
+        loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window' 
+      },
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader?presets[]=es2015', 
+        include: [path.resolve(__dirname, 'src')] 
+      },
+      { 
+        test: /\.css$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          'css-loader'
+        ]
+      }, 
+      { 
+        test: /\.less$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader', 'less-loader']
+      },
+      { 
+        test: /\.(woff|woff2)$/, 
+        loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { 
+        test: /\.ttf$/, 
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      { 
+        test: /\.eot$/, 
+        loader: "file-loader" },
+      { 
+        test: /\.svg$/, 
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml" 
+      }
     ]
   }
 };
