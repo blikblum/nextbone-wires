b0c954bb3ad285325e1df836da1057ba85dff6e3
Luiz Américo <luizamericop@gmail.com>
2018-06-07 21:03:00 -0300

    Add webpack clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,79 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+var CleanPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: __dirname + '/src/main.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: "bundle.js"
-  },
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: __dirname + '/dist/index.html',
-      template: __dirname + '/src/index.html'
-    }),
+var DIST_DIR = 'dist';
 
-    new MiniCSSExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  module: {
-    rules: [
-      { 
-        test: /\.hbs$/, 
-        loader: 'handlebars-loader' 
-      },
-      { 
-        test: /bootstrap.+\.js$/, 
-        loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window' 
-      },
-      { 
-        test: /\.js$/, 
-        loader: 'babel-loader?presets[]=es2015', 
-        include: [path.resolve(__dirname, 'src')] 
-      },
-      { 
-        test: /\.css$/,
-        use: [
-          MiniCSSExtractPlugin.loader,
-          'css-loader'
-        ]
-      }, 
-      { 
-        test: /\.less$/,
-        use: [MiniCSSExtractPlugin.loader, 'css-loader', 'less-loader']
-      },
-      { 
-        test: /\.(woff|woff2)$/, 
-        loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { 
-        test: /\.ttf$/, 
-        loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
-      { 
-        test: /\.eot$/, 
-        loader: "file-loader" },
-      { 
-        test: /\.svg$/, 
-        loader: "url-loader?limit=10000&mimetype=image/svg+xml" 
-      }
-    ]
-  }
-};
+var plugins = [
+  new HtmlWebpackPlugin({
+    filename: __dirname + '/dist/index.html',
+    template: __dirname + '/src/index.html'
+  }),
+
+  new MiniCSSExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: '[name].css',
+    chunkFilename: '[id].css'
+  })
+];
+
+module.exports = function (env) {
+
+  var isProd = env && env.production;
+
+  if (isProd) plugins.push(new CleanPlugin([DIST_DIR + '/*.*']));
+
+  return {
+    entry: __dirname + '/src/main.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: "bundle.js"
+    },
+    devtool: "source-map",
+    plugins: plugins,
+    module: {
+      rules: [
+        { 
+          test: /\.hbs$/, 
+          loader: 'handlebars-loader' 
+        },
+        { 
+          test: /bootstrap.+\.js$/, 
+          loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window' 
+        },
+        { 
+          test: /\.js$/, 
+          loader: 'babel-loader?presets[]=es2015', 
+          include: [path.resolve(__dirname, 'src')] 
+        },
+        { 
+          test: /\.css$/,
+          use: [
+            MiniCSSExtractPlugin.loader,
+            'css-loader'
+          ]
+        }, 
+        { 
+          test: /\.less$/,
+          use: [MiniCSSExtractPlugin.loader, 'css-loader', 'less-loader']
+        },
+        { 
+          test: /\.(woff|woff2)$/, 
+          loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+        { 
+          test: /\.ttf$/, 
+          loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+        { 
+          test: /\.eot$/, 
+          loader: "file-loader" },
+        { 
+          test: /\.svg$/, 
+          loader: "url-loader?limit=10000&mimetype=image/svg+xml" 
+        }
+      ]
+    }
+  };
+}
+
